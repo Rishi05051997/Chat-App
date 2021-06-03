1dd8192d1fa42fab175662c779f6cb50451cc372
vrushabhdhatrak <vrushabhdhatral10@gmail.com>
2021-06-03 07:57:07 +0530

    post form ui completed

--- frontend/chat-app/src/app/modules/streams.module.ts
+++ frontend/chat-app/src/app/modules/streams.module.ts
@@ -4,6 +4,8 @@ import { StreamsComponent } from '../components/streams/streams.component';
 import { TokenService } from '../services/token.service';
 import { ToolbarComponent } from '../components/toolbar/toolbar.component';
 import { SideComponent } from '../components/side/side.component';
+import { PostFormComponent } from '../components/post-form/post-form.component';
+import { PostsComponent } from '../components/posts/posts.component';
 
 
 
@@ -11,7 +13,9 @@ import { SideComponent } from '../components/side/side.component';
   declarations: [
     StreamsComponent,
     ToolbarComponent,
-    SideComponent
+    SideComponent,
+    PostFormComponent,
+    PostsComponent
   ],
   imports: [
     CommonModule
